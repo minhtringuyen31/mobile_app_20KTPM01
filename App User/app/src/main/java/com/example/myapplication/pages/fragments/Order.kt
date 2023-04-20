fc2ee11ea68d9e1ee5caab2610d8b239b1ccf20c
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2023-04-20 13:57:38 +0700

    display rating in product detail

--- App User/app/src/main/java/com/example/myapplication/pages/fragments/Order.kt
+++ App User/app/src/main/java/com/example/myapplication/pages/fragments/Order.kt
@@ -42,6 +42,7 @@ class Order : Fragment() {
     private lateinit var categoryViewModel: CategoryViewModel
     private lateinit var productViewModel: ProductViewModel
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
