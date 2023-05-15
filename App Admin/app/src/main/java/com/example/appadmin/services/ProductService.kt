1001749e3afac630d53c7c7a8457f39df7350f22
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2023-05-15 16:43:38 +0700

    Complete Image upload Product

--- App Admin/app/src/main/java/com/example/appadmin/services/ProductService.kt
+++ App Admin/app/src/main/java/com/example/appadmin/services/ProductService.kt
@@ -3,6 +3,8 @@ package com.example.appadmin.services
 import retrofit2.Call
 import retrofit2.http.*
 import com.example.appadmin.modals.Product
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface ProductService {
     @GET("product/{id}")
@@ -11,17 +13,49 @@ interface ProductService {
     @GET("product/")
     suspend fun getAllProduct(): List<Product>
 
+    @Multipart
     @POST("product/create")
-    suspend fun createProduct(@Body product: Product): Product
+    suspend fun createProduct(
+        @Part("name") name: RequestBody,
+        @Part("description") description: RequestBody,
+        @Part("size") size: RequestBody,
+        @Part("price_S") price_S: RequestBody,
+        @Part("price_M") price_M: RequestBody,
+        @Part("price_L") price_L: RequestBody,
+        @Part("status") status: RequestBody,
+        @Part("category_id") category_id: RequestBody,
+        @Part("update_date") update_date: RequestBody,
+        @Part("release_date") release_date: RequestBody,
+        @Part("sales") sales: RequestBody,
+        @Part image: MultipartBody.Part
+    ): Product
 
+    @Multipart
     @PUT("product/update/{id}")
-    suspend fun updateProduct(@Path("id") productId: Int, @Body product: Product): Product
+    suspend fun updateProduct(
+        @Path("id") productId: Int,
+        @Part("name") name: RequestBody,
+        @Part("description") description: RequestBody,
+        @Part("size") size: RequestBody,
+        @Part("price_S") price_S: RequestBody,
+        @Part("price_M") price_M: RequestBody,
+        @Part("price_L") price_L: RequestBody,
+        @Part("category_id") category_id: RequestBody,
+        @Part("update_date") update_date: RequestBody,
+        @Part image: MultipartBody.Part
+        ): Product
+
+    @PUT("product/updateWithoutImage/{id}")
+    suspend fun updateWithoutImage(
+        @Path("id") productId: Int,
+        @Body product: Product
+    ): Product
 
     @DELETE("product/delete/{id}")
     suspend fun deleteProduct(@Path("id") productId: Int): Boolean
 
     @PUT("product/disable/{id}")
-    suspend fun disableProduct(@Path("id") productId: Int):Boolean
+    suspend fun disableProduct(@Path("id") productId: Int): Boolean
 
     @PUT("product/enable/{id}")
     suspend fun enableProduct(@Path("id") productId: Int): Boolean
